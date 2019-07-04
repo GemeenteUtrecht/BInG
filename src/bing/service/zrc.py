3db3b3e9ed7c1b001d909d4bbad48d6644414bd8
Jorik Kraaikamp <jorikkraaikamp@gmail.com>
2019-07-04 14:52:32 +0200

    :bug: Fixed that the calls to the ZRC were working
    There was a problem with the new urls of the ZTC

--- src/bing/service/zrc.py
+++ src/bing/service/zrc.py
@@ -15,7 +15,7 @@ def fetch_zaak(url: str) -> Dict[str, Any]:
     config = BInGConfig.get_solo()
     zrc_client = get_zrc_client(
         scopes=["zds.scopes.zaken.lezen"],
-        zaaktypes=[config.zaaktype_vergadering, config.zaaktype_aanvraag],
+        zaaktypes=['*'],
     )
     zaak = zrc_client.retrieve("zaak", url=url)
     return zaak
@@ -42,7 +42,7 @@ def fetch_status(url: str) -> Dict[str, Any]:
     config = BInGConfig.get_solo()
     zrc_client = get_zrc_client(
         scopes=["zds.scopes.zaken.lezen"],
-        zaaktypes=[config.zaaktype_vergadering, config.zaaktype_aanvraag],
+        zaaktypes=['*'],
     )
     status = zrc_client.retrieve("status", url=url)
     return status
@@ -55,7 +55,7 @@ def fetch_resultaat(url: Optional[str]) -> Optional[Dict[str, str]]:
     config = BInGConfig.get_solo()
     zrc_client = get_zrc_client(
         scopes=["zds.scopes.zaken.lezen"],
-        zaaktypes=[config.zaaktype_vergadering, config.zaaktype_aanvraag],
+        zaaktypes=['*'],
     )
     resultaat = zrc_client.retrieve("resultaat", url=url)
     return resultaat
@@ -68,7 +68,7 @@ def set_status(zaak_url: str, statustype_url: str, **extra) -> Dict[str, str]:
     config = BInGConfig.get_solo()
     zrc_client = get_zrc_client(
         scopes=["zds.scopes.statussen.toevoegen"],
-        zaaktypes=[config.zaaktype_vergadering, config.zaaktype_aanvraag],
+        zaaktypes=['*'],
     )
 
     defaults = {"datumStatusGezet": timezone.now().isoformat()}
@@ -87,7 +87,7 @@ def set_resultaat(
     config = BInGConfig.get_solo()
     zrc_client = get_zrc_client(
         scopes=["zds.scopes.zaken.bijwerken"],
-        zaaktypes=[config.zaaktype_vergadering, config.zaaktype_aanvraag],
+        zaaktypes=['*'],
     )
 
     body = {
