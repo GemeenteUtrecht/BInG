8da99f06ad99ec02cea34cc0248cf9ee5a28b442
Sergei Maertens <sergei@maykinmedia.nl>
2019-07-29 17:38:46 +0200

    :sparkles: Show process definitions in admin

--- src/bing/camunda/management/commands/camunda_sync.py
+++ src/bing/camunda/management/commands/camunda_sync.py
@@ -1,7 +1,7 @@
 from django.core.management import BaseCommand
 
 from ...client import Camunda
-from ...models import Deployment
+from ...models import Deployment, ProcessDefinition
 
 
 class Command(BaseCommand):
@@ -14,4 +14,15 @@ class Command(BaseCommand):
         for deployment in deployments:
             Deployment.objects.update_or_create(
                 uuid=deployment["id"], defaults={"name": deployment["name"] or ""}
+            )
+
+        definitions = client.request("process-definition")
+        for definition in definitions:
+            ProcessDefinition.objects.update_or_create(
+                camunda_id=definition["id"],
+                defaults={
+                    "key": definition["key"],
+                    "name": definition["name"],
+                    "version": definition["version"],
+                },
             )
--- src/bing/camunda/models.py
+++ src/bing/camunda/models.py
@@ -12,3 +12,17 @@ class Deployment(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class ProcessDefinition(models.Model):
+    camunda_id = models.CharField(_("camunda id"), max_length=100, editable=False)
+    key = models.CharField(_("key"), max_length=100, editable=False)
+    name = models.CharField(_("name"), max_length=255, editable=False)
+    version = models.PositiveSmallIntegerField(_("version"), editable=False)
+
+    class Meta:
+        verbose_name = "process definition"
+        verbose_name_plural = "process definitions"
+
+    def __str__(self):
+        return self.name
