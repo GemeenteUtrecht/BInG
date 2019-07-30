3c7c5160f392da8ef1454a86944a3a9ab17be6de
Sergei Maertens <sergei@maykinmedia.nl>
2019-07-30 17:28:58 +0200

    :white_check_mark: Extend test with body check

--- src/bing/aanmeldformulier/tests/test_start_camunda.py
+++ src/bing/aanmeldformulier/tests/test_start_camunda.py
@@ -1,10 +1,13 @@
+import json
 import uuid
 
 from django.test import TestCase
 
 import requests_mock
+from freezegun import freeze_time
 
 from bing.config.models import APIConfig, BInGConfig
+from bing.meetings.tests.factories import MeetingFactory
 from bing.projects.tests.factories import ProjectAttachmentFactory, ProjectFactory
 
 from ..tasks import start_camunda_process
@@ -17,17 +20,22 @@ class CamundaStartTests(TestCase):
         api_config.camunda_root = "http://camunda.utrecht.nl/"
         api_config.save()
 
+        cls.zaaktype = f"https://ztc.utrecht.nl/zaaktype/{uuid.uuid4()}"
+
         bing_config = BInGConfig.get_solo()
+        bing_config.zaaktype_aanvraag = cls.zaaktype
         bing_config.aanvraag_process_key = "bing"
         bing_config.save()
 
+    @freeze_time("2019-07-30 14:00")
     def test_full_project(self):
         """
         Test the Camunda API interaction from BInG.
 
         See: https://docs.camunda.org/manual/7.9/reference/rest/process-definition/post-start-process-instance/
         """
-        project = ProjectFactory.create()
+        meeting = MeetingFactory.create()
+        project = ProjectFactory.create(meeting=meeting)
         attachment = ProjectAttachmentFactory.create(project=project)
 
         # kick off the thing
@@ -55,3 +63,31 @@ class CamundaStartTests(TestCase):
 
         request = m.last_request
         self.assertIsNotNone(request)
+
+        # check request contents
+        self.assertEqual(
+            request.url,
+            "http://camunda.utrecht.nl/engine-rest/process-definition/key/bing/start",
+        )
+
+        expected_body = {
+            "variables": {
+                "zaaktype": {"value": self.zaaktype, "type": "String"},
+                "project_id": {"value": project.project_id, "type": "String"},
+                "datum_ingediend": {"value": "2019-07-30", "type": "Date"},
+                "naam": {"value": project.name, "type": "String"},
+                "toetswijze": {"value": project.toetswijze, "type": "String"},
+                "documenten": {
+                    "value": json.dumps([attachment.eio_url]),
+                    "type": "Json",
+                },
+                "meeting_datum": {
+                    "value": meeting.start.date().isoformat(),
+                    "type": "Date",
+                },
+                "meeting_zaak": {"value": meeting.zaak, "type": "String"},
+            },
+            "businessKey": f"bing-project-{project.project_id}",
+            "withVariablesInReturn": False,
+        }
+        self.assertEqual(request.json(), expected_body)
