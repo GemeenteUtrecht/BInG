1975f1018ebbedd97f9df2abafa0046485df2293
Sergei Maertens <sergei@maykinmedia.nl>
2020-03-03 15:44:14 +0100

    :construction: Figure out tile pixel coordinates...

--- src/bing/js/components/map/index.js
+++ src/bing/js/components/map/index.js
@@ -4,11 +4,14 @@
 
 import * as L from 'leaflet';
 
+import { getFeatureInfo, getTile } from './features.js';
+
 const BASE_URL = 'https://geodata.nationaalgeoregister.nl/tiles/service';
+const attribution = 'Kaartgegevens &copy; <a href="https://www.kadaster.nl">Kadaster</a> | <a href="https://www.verbeterdekaart.nl">Verbeter de kaart</a>';
 
 // 3857 -> pseudo mercator = WGS 84
-const TILE_LAYER_URL = `${BASE_URL}/wmts/brtachtergrondkaart/EPSG:3857/{z}/{x}/{y}.png`;
-
+const BRT_TILE_LAYER_URL = `${BASE_URL}/wmts/brtachtergrondkaart/EPSG:3857/{z}/{x}/{y}.png`;
+const BGT_TILE_LAYER_URL = `${BASE_URL}/wmts/bgtachtergrond/EPSG:28992/{z}/{x}/{y}.png`;
 
 const CENTER = {
   latitude: 52.093249,
@@ -22,6 +25,8 @@ class Map {
   constructor(node) {
     this.node = node;
     this._map = this.init(node);
+
+    this._zoomNode = document.getElementById('zoomLevel');
   }
 
   init(node) {
@@ -34,10 +39,9 @@ class Map {
 
     const map = L.map(node, options);
     const tileLayer = L.tileLayer(
-      TILE_LAYER_URL,
+      BRT_TILE_LAYER_URL,
       {
-        attribution: "Kaartgegevens &copy; <a href='https://www.kadaster.nl'>Kadaster</a> | \
-        <a href='https://www.verbeterdekaart.nl'>Verbeter de kaart</a>",
+        attribution: attribution,
         minZoom: 6,
         maxZoom: 19,
         // type: 'wmts',
@@ -45,8 +49,32 @@ class Map {
     );
 
     map.addLayer(tileLayer);
+    this._tileLayer = tileLayer;
+
+    map.on('click', (e) => this.onClick(e));
+    map.on('zoomend', () => {
+      this._zoomNode.innerText = this._map.getZoom();
+    });
 
     return map;
+  }
+
+  onClick(event) {
+    const lng = event.latlng.lng;
+    const lat = event.latlng.lat;
+    const z = this._map.getZoom();
+
+    const {xTile, yTile} = getTile(lng, lat, z);
+
+    const {x, y} = this._tileLayer.getTileSize();
+
+    const relativeX = event.containerPoint.x % x;
+    const relativeY = event.containerPoint.y % y;
+
+    console.log(relativeX, relativeY);
+
+    const features = getFeatureInfo(xTile, yTile, z, 0, 0);
+    console.log(features);
   }
 }
 
